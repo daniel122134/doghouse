d7110ee6711943feeb2032eb199ce825c83e4fde
daniel haddad <daniel.haddad@yahooinc.com>
2023-08-12 12:00:45 +0300

    added skelaton

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require('cors')
+
+const app = express()
+const port = 8080
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json({ limit: 1024 *1024 * 5 }))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true
+  })
+)
+
+app.use('/public', express.static(path.join(__dirname, '..', 'frontend', 'public')))
+
+app.use(
+  express.static(path.join(__dirname, '..', 'frontend', 'dist'))
+)
+
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'dist'))
+})
+
+
+
+
+app.listen(port, () => {
+  console.info(`Example app listening on port ${port}`)
+})
+
