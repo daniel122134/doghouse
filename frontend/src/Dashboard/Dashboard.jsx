235f6ab3800fe6bbe653d312273c2fd10d70f46c
Hadar Libman <hadar.libman@post.idc.ac.il>
2023-08-13 21:46:10 +0300

    poles page front

--- frontend/src/Dashboard/Dashboard.jsx
+++ frontend/src/Dashboard/Dashboard.jsx
@@ -5,6 +5,10 @@ import relativeTime from 'dayjs/plugin/relativeTime'
 import Sidebar from '../sidebar/Sidebar'
 import Icon from "../components/Icon.jsx";
 import ProfilePage from "../ProfilePage/ProfilePage.jsx";
+import ExplorePage from "../ExplorePage/ExplorePage.jsx";
+import PeePolesPage from "../PeePolesPage/PeePolesPage.jsx";
+import DogedexPage from "../DogedexPage/DogedexPage.jsx";
+import AdminPage from "../AdminPage/AdminPage.jsx";
 
 dayjs.extend(relativeTime)
 
@@ -59,12 +63,23 @@ function App() {
               <Icon icon="fa fa-spinner fa-spin"></Icon>
             </div>
           ) : null}
-          {!isInitialLoading && currentPage === 'Profile' ? (
+          {!isInitialLoading && currentPage === 'ProfilePage' ? (
             <ProfilePage />
           ) : null}
-
           {!isInitialLoading && currentPage === 'FeedPage' ? (
             <ProfilePage />
+          ) : null}
+          {!isInitialLoading && currentPage === 'ExplorePage' ? (
+              <ExplorePage />
+          ) : null}
+          {!isInitialLoading && currentPage === 'PeePolesPage' ? (
+              <PeePolesPage />
+          ) : null}
+          {!isInitialLoading && currentPage === 'DogedexPage' ? (
+              <DogedexPage />
+          ) : null}
+          {!isInitialLoading && currentPage === 'AdminPage' ? (
+              <AdminPage />
           ) : null}
           
         </div>
