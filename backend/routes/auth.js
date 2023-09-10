9b58e870c4b77517eece66d7a88de8943277184a
daniel haddad <daniel.haddad@yahooinc.com>
2023-09-10 12:04:48 +0300

    skelaton for the rest of the requests

---
+++ backend/routes/auth.js
@@ -0,0 +1,8 @@
+import express from "express";
+import {dal} from "../DAL/persist.js";
+import authJwt from "../middleware/authJwt.js";
+
+const router = express.Router()
+
+
+export default router
---
+++ backend/routes/posts.js
@@ -0,0 +1,7 @@
+import express from "express";
+import {dal} from "../DAL/persist.js";
+import authJwt from "../middleware/authJwt.js";
+
+const router = express.Router()
+
+export default router
