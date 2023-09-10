2789acf5d3f2fd09aa4c3a3a43d933c6d6b453c5
daniel haddad <daniel.haddad@yahooinc.com>
2023-09-10 20:47:57 +0300

    added setup and teardown

--- backend/tests/tests.js
+++ backend/tests/tests.js
@@ -1,15 +1,86 @@
-
 import {apiUtils} from "./apiUtils.js";
 import assert from "assert";
+import {dal} from "../DAL/persist.js";
+
+async function clearTestData() {
+  try {
+    const allUsers = await dal.getAllUsers()
+    const testUserId = allUsers.find(user => user.username === "test").id
+    const followedUserId = allUsers.find(user => user.username === "followed").id
+    const unfollowedUserId = allUsers.find(user => user.username === "unfollowed").id
+
+    const userPosts = await dal.getAllUserFollowsPosts(testUserId)
+    const testPostId = userPosts[0].id
+
+    try {
+      await dal.removeLike(testUserId, testPostId)
+    } catch (e) {
+      console.log(e)
+    }
+
+    try {
+      await dal.deletePost(testPostId)
+    } catch (e) {
+      console.log(e)
+    }
+
+    try {
+      await dal.deleteUser(testUserId)
+    } catch (e) {
+      console.log(e)
+    }
+
+    try {
+      await dal.deleteUser(followedUserId)
+    } catch (e) {
+      console.log(e)
+    }
+
+    try {
+      await dal.deleteUser(unfollowedUserId)
+    } catch (e) {
+      console.log(e)
+    }
+
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+beforeEach(async function () {
+  console.log('setup Starting')
+
+  try {
+    await dal.createUser("test", "test", "test")
+    await dal.createUser("followed", "followed", "followed")
+    await dal.createUser("unfollowed", "unfollowed", "unfollowed")
+    const allUsers = await dal.getAllUsers()
+    const testUserId = allUsers.find(user => user.username === "test").id
+    const followedUserId = allUsers.find(user => user.username === "followed").id
+    const unfollowedUserId = allUsers.find(user => user.username === "unfollowed").id
+    await dal.createPost(testUserId, "test")
+    const userPosts = await dal.getAllUserFollowsPosts(testUserId)
+    const testPostId = userPosts[0].id
+    await dal.addLike(testUserId, testPostId)
+    await dal.followUser(testUserId, followedUserId)
+  } catch (e) {
+    console.log(e)
+  }
+});
 
 
-describe('POST /api/auth/login', function() {
-  it('should return a token', function() {
+afterEach(async function () {
+  console.log('Teardown Running')
+  await clearTestData()
+});
+
+describe('POST /api/auth/login', function () {
+  it('should return a token', function () {
     return new Promise((resolve, reject) => {
       apiUtils.login("test", "test").then(async (response) => {
         assert(response.status === 200)
         const json = await response.json()
-        assert(json.token)
+        assert(typeof json.id === "number")
         resolve()
       })
     })
@@ -17,8 +88,8 @@ describe('POST /api/auth/login', function() {
 })
 
 //test logout
-describe('POST /api/auth/logout', function() {
-  it('should return a 200', function() {
+describe('POST /api/auth/logout', function () {
+  it('should return a 200', function () {
     return new Promise((resolve, reject) => {
       apiUtils.logout().then(async (response) => {
         assert(response.status === 200)
@@ -29,8 +100,8 @@ describe('POST /api/auth/logout', function() {
 })
 
 //test signup
-describe('POST /api/user', function() {
-  it('should return a user object', function() {
+describe('POST /api/user', function () {
+  it('should return a user object', function () {
     return new Promise((resolve, reject) => {
       apiUtils.signup("test", "test", "test").then(async (response) => {
         assert(response.status === 200)
@@ -46,8 +117,8 @@ describe('POST /api/user', function() {
 })
 
 //test get feature state
-describe('GET /api/admin/feature/:featureName', function() {
-  it('should return a feature object', function() {
+describe('GET /api/admin/feature/:featureName', function () {
+  it('should return a feature object', function () {
     return new Promise((resolve, reject) => {
       apiUtils.getFeatureState("test").then(async (response) => {
         assert(response.status === 200)
@@ -61,8 +132,8 @@ describe('GET /api/admin/feature/:featureName', function() {
 
 
 //test set feature state
-describe('PUT /api/admin/feature/:featureName', function() {
-  it('should return a feature object', function() {
+describe('PUT /api/admin/feature/:featureName', function () {
+  it('should return a feature object', function () {
     return new Promise((resolve, reject) => {
       apiUtils.setFeatureState("test", 1).then(async (response) => {
         assert(response.status === 200)
@@ -75,8 +146,8 @@ describe('PUT /api/admin/feature/:featureName', function() {
 })
 
 //test set pole owner
-describe('PUT /api/pee/:poleName/owner', function() {
-  it('should return a pole object', function() {
+describe('PUT /api/pee/:poleName/owner', function () {
+  it('should return a pole object', function () {
     return new Promise((resolve, reject) => {
       apiUtils.setPeePoleOwner("test", 1).then(async (response) => {
         assert(response.status === 200)
@@ -89,8 +160,8 @@ describe('PUT /api/pee/:poleName/owner', function() {
 })
 
 //test get all poles
-describe('GET /api/pee/allPoles', function() {
-  it('should return a pole object', function() {
+describe('GET /api/pee/allPoles', function () {
+  it('should return a pole object', function () {
     return new Promise((resolve, reject) => {
       apiUtils.getAllPoles().then(async (response) => {
         assert(response.status === 200)
@@ -106,8 +177,8 @@ describe('GET /api/pee/allPoles', function() {
 })
 
 //test get pole owner
-describe('GET /api/pee/:poleName/owner', function() {
-  it('should return a pole object', function() {
+describe('GET /api/pee/:poleName/owner', function () {
+  it('should return a pole object', function () {
     return new Promise((resolve, reject) => {
       apiUtils.getPoleOwner("fence").then(async (response) => {
         assert(response.status === 200)
@@ -120,8 +191,8 @@ describe('GET /api/pee/:poleName/owner', function() {
 })
 
 // test get user data
-describe('GET /api/user', function() {
-  it('should return a user object', function() {
+describe('GET /api/user', function () {
+  it('should return a user object', function () {
     return new Promise((resolve, reject) => {
       apiUtils.getUserData().then(async (response) => {
         assert(response.status === 200)
@@ -138,8 +209,8 @@ describe('GET /api/user', function() {
 
 
 // test update user data
-describe('PUT /api/user', function() {
-  it('should return a user object', function() {
+describe('PUT /api/user', function () {
+  it('should return a user object', function () {
     return new Promise((resolve, reject) => {
       apiUtils.updateUserData("test", "test", "test").then(async (response) => {
         assert(response.status === 200)
@@ -155,8 +226,8 @@ describe('PUT /api/user', function() {
 })
 
 // test edit post content
-describe('PUT /api/post/:postId', function() {
-  it('should return a post object', function() {
+describe('PUT /api/post/:postId', function () {
+  it('should return a post object', function () {
     return new Promise((resolve, reject) => {
       apiUtils.editPostContent(1, "test").then(async (response) => {
         assert(response.status === 200)
@@ -171,8 +242,8 @@ describe('PUT /api/post/:postId', function() {
 
 
 // test get post update time
-describe('GET /api/post/:postId/updated', function() {
-  it('should return a post object', function() {
+describe('GET /api/post/:postId/updated', function () {
+  it('should return a post object', function () {
     return new Promise((resolve, reject) => {
       apiUtils.getPostUpdateTime(1).then(async (response) => {
         assert(response.status === 200)
@@ -186,8 +257,8 @@ describe('GET /api/post/:postId/updated', function() {
 })
 
 // test addLike
-describe('PUT /api/post/:postId/like', function() {
-  it('should return a post object', function() {
+describe('PUT /api/post/:postId/like', function () {
+  it('should return a post object', function () {
     return new Promise((resolve, reject) => {
       apiUtils.addLike(1).then(async (response) => {
         assert(response.status === 200)
@@ -202,8 +273,8 @@ describe('PUT /api/post/:postId/like', function() {
 
 
 //test removeLike
-describe('DELETE /api/post/:postId/like', function() {
-  it('should return a post object', function() {
+describe('DELETE /api/post/:postId/like', function () {
+  it('should return a post object', function () {
     return new Promise((resolve, reject) => {
       apiUtils.removeLike(1).then(async (response) => {
         assert(response.status === 200)
@@ -217,8 +288,8 @@ describe('DELETE /api/post/:postId/like', function() {
 })
 
 //test get post likes number
-describe('GET /api/post/:postId/likes', function() {
-  it('should return a post object', function() {
+describe('GET /api/post/:postId/likes', function () {
+  it('should return a post object', function () {
     return new Promise((resolve, reject) => {
       apiUtils.getPostLikeNumber(1).then(async (response) => {
         assert(response.status === 200)
@@ -232,8 +303,8 @@ describe('GET /api/post/:postId/likes', function() {
 })
 
 //test get post like number by user
-describe('GET /api/post/:postId/likes/:userId', function() {
-  it('should return a post object', function() {
+describe('GET /api/post/:postId/likes/:userId', function () {
+  it('should return a post object', function () {
     return new Promise((resolve, reject) => {
       apiUtils.getPostLikeNumberByUser(1, 1).then(async (response) => {
         assert(response.status === 200)
@@ -247,8 +318,8 @@ describe('GET /api/post/:postId/likes/:userId', function() {
 })
 
 //test get all users not followed by user
-describe('GET /api/user/:userId/notFollowed', function() {
-  it('should return a user object', function() {
+describe('GET /api/user/:userId/notFollowed', function () {
+  it('should return a user object', function () {
     return new Promise((resolve, reject) => {
       apiUtils.getAllUsersNotFollowedByUser(1).then(async (response) => {
         assert(response.status === 200)
@@ -264,8 +335,8 @@ describe('GET /api/user/:userId/notFollowed', function() {
 })
 
 //test get all users followed by user
-describe('GET /api/user/:userId/followed', function() {
-  it('should return a user object', function() {
+describe('GET /api/user/:userId/followed', function () {
+  it('should return a user object', function () {
     return new Promise((resolve, reject) => {
       apiUtils.getAllUsersFollowedByUser(1).then(async (response) => {
         assert(response.status === 200)
@@ -281,8 +352,8 @@ describe('GET /api/user/:userId/followed', function() {
 })
 
 //test follow user
-describe('PUT /api/follow/:userId', function() {
-  it('should return a user object', function() {
+describe('PUT /api/follow/:userId', function () {
+  it('should return a user object', function () {
     return new Promise((resolve, reject) => {
       apiUtils.followUser(1, 1).then(async (response) => {
         assert(response.status === 200)
@@ -296,8 +367,8 @@ describe('PUT /api/follow/:userId', function() {
 })
 
 //test unfollow user
-describe('DELETE /api/follow/:userId', function() {
-  it('should return a user object', function() {
+describe('DELETE /api/follow/:userId', function () {
+  it('should return a user object', function () {
     return new Promise((resolve, reject) => {
       apiUtils.unfollowUser(1, 1).then(async (response) => {
         assert(response.status === 200)
@@ -311,8 +382,8 @@ describe('DELETE /api/follow/:userId', function() {
 })
 
 //test get all users matching prefix
-describe('GET /api/user/:prefix', function() {
-  it('should return a user object', function() {
+describe('GET /api/user/:prefix', function () {
+  it('should return a user object', function () {
     return new Promise((resolve, reject) => {
       apiUtils.getAllUsersMatchingPrefix("a").then(async (response) => {
         assert(response.status === 200)
@@ -328,8 +399,8 @@ describe('GET /api/user/:prefix', function() {
 })
 
 //test get all users matching substring
-describe('GET /api/user/:substring', function() {
-  it('should return a user object', function() {
+describe('GET /api/user/:substring', function () {
+  it('should return a user object', function () {
     return new Promise((resolve, reject) => {
       apiUtils.getAllUsersMatchingSubstring("a").then(async (response) => {
         assert(response.status === 200)
@@ -346,8 +417,8 @@ describe('GET /api/user/:substring', function() {
 
 
 //test get is following
-describe('GET /api/follow/:userId', function() {
-  it('should return a user object', function() {
+describe('GET /api/follow/:userId', function () {
+  it('should return a user object', function () {
     return new Promise((resolve, reject) => {
       apiUtils.getIsFollowing(1, 1).then(async (response) => {
         assert(response.status === 200)
@@ -360,8 +431,8 @@ describe('GET /api/follow/:userId', function() {
   });
 })
 
-describe('GET /api/user/all', function() {
-  it('should return array of user ids', function() {
+describe('GET /api/user/all', function () {
+  it('should return array of user ids', function () {
     return new Promise((resolve, reject) => {
       apiUtils.getAllUsers().then(async (response) => {
         assert(response.status === 200)
@@ -376,8 +447,8 @@ describe('GET /api/user/all', function() {
   });
 })
 
-describe('GET /api/admin/features', function() {
-  it('should return a feature dict', function() {
+describe('GET /api/admin/features', function () {
+  it('should return a feature dict', function () {
     return new Promise((resolve, reject) => {
       apiUtils.getFeatures().then(async (response) => {
         assert.strictEqual(response.status, 200);
@@ -394,8 +465,8 @@ describe('GET /api/admin/features', function() {
 });
 
 
-describe('GET /api/admin/eventLogs', function() {
-  it('should return array of event logs', function() {
+describe('GET /api/admin/eventLogs', function () {
+  it('should return array of event logs', function () {
     return new Promise((resolve, reject) => {
       apiUtils.getEventLogs().then(async (response) => {
         assert.strictEqual(response.status, 200);
@@ -414,8 +485,8 @@ describe('GET /api/admin/eventLogs', function() {
 });
 
 
-describe('GET /api/posts/list/followed', function() {
-  it('should return array of posts', function() {
+describe('GET /api/posts/list/followed', function () {
+  it('should return array of posts', function () {
     return new Promise((resolve, reject) => {
       apiUtils.getAllUserFollowsPosts().then(async (response) => {
         assert.strictEqual(response.status, 200);
@@ -435,8 +506,8 @@ describe('GET /api/posts/list/followed', function() {
 });
 
 
-describe('POST /api/posts', function() {
-  it('should create a new post', function() {
+describe('POST /api/posts', function () {
+  it('should create a new post', function () {
     const content = "Test post content";
     return new Promise((resolve, reject) => {
       apiUtils.createPost(content).then(async (response) => {
@@ -450,8 +521,8 @@ describe('POST /api/posts', function() {
   });
 });
 
-describe('GET /api/pee/allPoles', function() {
-  it('should return array of pee poles', function() {
+describe('GET /api/pee/allPoles', function () {
+  it('should return array of pee poles', function () {
     return new Promise((resolve, reject) => {
       apiUtils.getAllPoles().then(async (response) => {
         assert.strictEqual(response.status, 200);
@@ -470,8 +541,8 @@ describe('GET /api/pee/allPoles', function() {
 });
 
 //******************
-describe('GET /api/follow/${userId}', function() {
-  it('should return array of posts', function() {
+describe('GET /api/follow/${userId}', function () {
+  it('should return array of posts', function () {
     return new Promise((resolve, reject) => {
       apiUtils.getAllUserFollowsPosts().then(async (response) => {
         assert.strictEqual(response.status, 200);
@@ -490,8 +561,8 @@ describe('GET /api/follow/${userId}', function() {
   });
 });
 
-describe('DELETE /api/user/${userId}', function() {
-  it('should delete a user', function() {
+describe('DELETE /api/user/${userId}', function () {
+  it('should delete a user', function () {
     return new Promise((resolve, reject) => {
       apiUtils.deleteUser(1).then(async (response) => {
         assert.strictEqual(response.status, 200);
