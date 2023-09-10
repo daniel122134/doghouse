4ebb8c5604219615b58b5736ebc3ed37a402200e
daniel haddad <daniel.haddad@yahooinc.com>
2023-09-10 20:22:35 +0300

    added lots of tests

--- backend/tests/tests.js
+++ backend/tests/tests.js
@@ -2,6 +2,364 @@
 import {apiUtils} from "./apiUtils.js";
 import assert from "assert";
 
+
+describe('POST /api/auth/login', function() {
+  it('should return a token', function() {
+    return new Promise((resolve, reject) => {
+      apiUtils.login("test", "test").then(async (response) => {
+        assert(response.status === 200)
+        const json = await response.json()
+        assert(json.token)
+        resolve()
+      })
+    })
+  });
+})
+
+//test logout
+describe('POST /api/auth/logout', function() {
+  it('should return a 200', function() {
+    return new Promise((resolve, reject) => {
+      apiUtils.logout().then(async (response) => {
+        assert(response.status === 200)
+        resolve()
+      })
+    })
+  });
+})
+
+//test signup
+describe('POST /api/user', function() {
+  it('should return a user object', function() {
+    return new Promise((resolve, reject) => {
+      apiUtils.signup("test", "test", "test").then(async (response) => {
+        assert(response.status === 200)
+        const json = await response.json()
+        assert(json.id)
+        assert(json.username === "test")
+        assert(json.email === "test")
+        assert(json.isAdmin === false)
+        resolve()
+      })
+    })
+  });
+})
+
+//test get feature state
+describe('GET /api/admin/feature/:featureName', function() {
+  it('should return a feature object', function() {
+    return new Promise((resolve, reject) => {
+      apiUtils.getFeatureState("test").then(async (response) => {
+        assert(response.status === 200)
+        const json = await response.json()
+        assert(json.state === 0)
+        resolve()
+      })
+    })
+  });
+})
+
+
+//test set feature state
+describe('PUT /api/admin/feature/:featureName', function() {
+  it('should return a feature object', function() {
+    return new Promise((resolve, reject) => {
+      apiUtils.setFeatureState("test", 1).then(async (response) => {
+        assert(response.status === 200)
+        const json = await response.json()
+        assert(json.state === 1)
+        resolve()
+      })
+    })
+  });
+})
+
+//test set pole owner
+describe('PUT /api/pee/:poleName/owner', function() {
+  it('should return a pole object', function() {
+    return new Promise((resolve, reject) => {
+      apiUtils.setPeePoleOwner("test", 1).then(async (response) => {
+        assert(response.status === 200)
+        const json = await response.json()
+        assert(json.ownerId === 1)
+        resolve()
+      })
+    })
+  });
+})
+
+//test get all poles
+describe('GET /api/pee/allPoles', function() {
+  it('should return a pole object', function() {
+    return new Promise((resolve, reject) => {
+      apiUtils.getAllPoles().then(async (response) => {
+        assert(response.status === 200)
+        const json = await response.json()
+        assert(json instanceof Array)
+        for (let pole of json) {
+          assert(typeof pole === "string")
+        }
+        resolve()
+      })
+    })
+  });
+})
+
+//test get pole owner
+describe('GET /api/pee/:poleName/owner', function() {
+  it('should return a pole object', function() {
+    return new Promise((resolve, reject) => {
+      apiUtils.getPoleOwner("fence").then(async (response) => {
+        assert(response.status === 200)
+        const json = await response.json()
+        assert(json.ownerId === 1)
+        resolve()
+      })
+    })
+  });
+})
+
+// test get user data
+describe('GET /api/user', function() {
+  it('should return a user object', function() {
+    return new Promise((resolve, reject) => {
+      apiUtils.getUserData().then(async (response) => {
+        assert(response.status === 200)
+        const json = await response.json()
+        assert(json.id)
+        assert(json.username === "test")
+        assert(json.email === "test")
+        assert(json.isAdmin === false)
+        resolve()
+      })
+    })
+  });
+})
+
+
+// test update user data
+describe('PUT /api/user', function() {
+  it('should return a user object', function() {
+    return new Promise((resolve, reject) => {
+      apiUtils.updateUserData("test", "test", "test").then(async (response) => {
+        assert(response.status === 200)
+        const json = await response.json()
+        assert(json.id)
+        assert(json.username === "test")
+        assert(json.email === "test")
+        assert(json.isAdmin === false)
+        resolve()
+      })
+    })
+  });
+})
+
+// test edit post content
+describe('PUT /api/post/:postId', function() {
+  it('should return a post object', function() {
+    return new Promise((resolve, reject) => {
+      apiUtils.editPostContent(1, "test").then(async (response) => {
+        assert(response.status === 200)
+        const json = await response.json()
+        assert(json.id)
+        assert(json.content === "test")
+        resolve()
+      })
+    })
+  });
+})
+
+
+// test get post update time
+describe('GET /api/post/:postId/updated', function() {
+  it('should return a post object', function() {
+    return new Promise((resolve, reject) => {
+      apiUtils.getPostUpdateTime(1).then(async (response) => {
+        assert(response.status === 200)
+        const json = await response.json()
+        assert(json.id)
+        assert(json.updated)
+        resolve()
+      })
+    })
+  });
+})
+
+// test addLike
+describe('PUT /api/post/:postId/like', function() {
+  it('should return a post object', function() {
+    return new Promise((resolve, reject) => {
+      apiUtils.addLike(1).then(async (response) => {
+        assert(response.status === 200)
+        const json = await response.json()
+        assert(json.id)
+        assert(json.likes === 1)
+        resolve()
+      })
+    })
+  });
+})
+
+
+//test removeLike
+describe('DELETE /api/post/:postId/like', function() {
+  it('should return a post object', function() {
+    return new Promise((resolve, reject) => {
+      apiUtils.removeLike(1).then(async (response) => {
+        assert(response.status === 200)
+        const json = await response.json()
+        assert(json.id)
+        assert(json.likes === 0)
+        resolve()
+      })
+    })
+  });
+})
+
+//test get post likes number
+describe('GET /api/post/:postId/likes', function() {
+  it('should return a post object', function() {
+    return new Promise((resolve, reject) => {
+      apiUtils.getPostLikeNumber(1).then(async (response) => {
+        assert(response.status === 200)
+        const json = await response.json()
+        assert(json.id)
+        assert(json.likes === 0)
+        resolve()
+      })
+    })
+  });
+})
+
+//test get post like number by user
+describe('GET /api/post/:postId/likes/:userId', function() {
+  it('should return a post object', function() {
+    return new Promise((resolve, reject) => {
+      apiUtils.getPostLikeNumberByUser(1, 1).then(async (response) => {
+        assert(response.status === 200)
+        const json = await response.json()
+        assert(json.id)
+        assert(json.likes === 0)
+        resolve()
+      })
+    })
+  });
+})
+
+//test get all users not followed by user
+describe('GET /api/user/:userId/notFollowed', function() {
+  it('should return a user object', function() {
+    return new Promise((resolve, reject) => {
+      apiUtils.getAllUsersNotFollowedByUser(1).then(async (response) => {
+        assert(response.status === 200)
+        const json = await response.json()
+        assert(json instanceof Array)
+        for (let user of json) {
+          assert(typeof user === "number")
+        }
+        resolve()
+      })
+    })
+  });
+})
+
+//test get all users followed by user
+describe('GET /api/user/:userId/followed', function() {
+  it('should return a user object', function() {
+    return new Promise((resolve, reject) => {
+      apiUtils.getAllUsersFollowedByUser(1).then(async (response) => {
+        assert(response.status === 200)
+        const json = await response.json()
+        assert(json instanceof Array)
+        for (let user of json) {
+          assert(typeof user === "number")
+        }
+        resolve()
+      })
+    })
+  });
+})
+
+//test follow user
+describe('PUT /api/follow/:userId', function() {
+  it('should return a user object', function() {
+    return new Promise((resolve, reject) => {
+      apiUtils.followUser(1, 1).then(async (response) => {
+        assert(response.status === 200)
+        const json = await response.json()
+        assert(json.id)
+        assert(json.following === 1)
+        resolve()
+      })
+    })
+  });
+})
+
+//test unfollow user
+describe('DELETE /api/follow/:userId', function() {
+  it('should return a user object', function() {
+    return new Promise((resolve, reject) => {
+      apiUtils.unfollowUser(1, 1).then(async (response) => {
+        assert(response.status === 200)
+        const json = await response.json()
+        assert(json.id)
+        assert(json.following === 0)
+        resolve()
+      })
+    })
+  });
+})
+
+//test get all users matching prefix
+describe('GET /api/user/:prefix', function() {
+  it('should return a user object', function() {
+    return new Promise((resolve, reject) => {
+      apiUtils.getAllUsersMatchingPrefix("a").then(async (response) => {
+        assert(response.status === 200)
+        const json = await response.json()
+        assert(json instanceof Array)
+        for (let user of json) {
+          assert(typeof user === "number")
+        }
+        resolve()
+      })
+    })
+  });
+})
+
+//test get all users matching substring
+describe('GET /api/user/:substring', function() {
+  it('should return a user object', function() {
+    return new Promise((resolve, reject) => {
+      apiUtils.getAllUsersMatchingSubstring("a").then(async (response) => {
+        assert(response.status === 200)
+        const json = await response.json()
+        assert(json instanceof Array)
+        for (let user of json) {
+          assert(typeof user === "number")
+        }
+        resolve()
+      })
+    })
+  });
+})
+
+
+//test get is following
+describe('GET /api/follow/:userId', function() {
+  it('should return a user object', function() {
+    return new Promise((resolve, reject) => {
+      apiUtils.getIsFollowing(1, 1).then(async (response) => {
+        assert(response.status === 200)
+        const json = await response.json()
+        assert(json.id)
+        assert(json.following === 0)
+        resolve()
+      })
+    })
+  });
+})
+
 describe('GET /api/user/all', function() {
   it('should return array of user ids', function() {
     return new Promise((resolve, reject) => {
